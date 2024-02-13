79d940c8c7a79c35eb2cf0b73889bd30390d10f4
yaroslav <yaroslav.voronovskyi@gmail.com>
2024-02-13 15:58:14 +0200

    Complited seventh FrontendHomework

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 1. Create Button component with onIncrease event
 2. Pass multiplier prop inside of Button component
-3. Create Counter component that includes few Button components
+3. Create Counter component that includes few Button 
+components
 4. Create ToDo list
--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import { Component } from 'react';
+import { useState } from "react";
+import TodoInput from "./TodoInput";
+import TodoList from "./TodoList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Task 1-3
+class Button extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			counter: 0,
+		};
+	}
+
+	onIncreaseEvent = () => {
+		this.setState({
+			counter: this.state.counter + 1
+		})
+	}
+
+	render() {
+		const value = this.props.title;
+		return (
+			<div className='Button'>{value}
+				<br />
+				<span>{this.state.counter} </span>
+				<br />
+				<button onClick={this.onIncreaseEvent}>click</button>
+				<br />
+				<span>{this.state.counter}</span>
+				<br />
+				<button onClick={this.onIncreaseEvent}>click</button>
+				<br />
+				<span>{this.state.counter}</span>
+				<br />
+				<button onClick={this.onIncreaseEvent}>click</button>
+			</div>
+		)
+	}
 }
 
-export default App;
+class PropsSender extends Component {
+	state = {
+		title: 'Hello you from PropsSender component',
+	};
+
+	render() {
+		return <Button title={this.state.title} />;
+	}
+}
+
+// Task Todo list with class components
+const App = () => {
+
+	const [todo, setTodo] = useState("");
+
+	const [todos, setTodos] = useState([]);
+
+	const addTodo = () => {
+		if (todo !== '') {
+			setTodos([...todos, todo]);
+			setTodo('');
+		}
+	}
+
+	const deleteTodo = (text) => {
+		const newTodos = todos.filter((todo) => {
+			return todo !== text;
+		});
+		setTodos(newTodos);
+	};
+
+	return (
+		<div className="App">
+			<h1>React Todo App</h1>
+			<TodoInput todo={todo} setTodo={setTodo} addTodo={addTodo} />
+			<TodoList list={todos} remove={deleteTodo} />
+		</div>
+	);
+};
+
+
+// Task Toto list with functions
+function TodoListFuntion() {
+	const [todos, setTodos] = useState([])
+	const [inputValue, setInputValue] = useState('')
+
+	function handleChange(e) {
+		setInputValue(e.target.value)
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault()
+		setTodos([...todos, inputValue])
+		setInputValue('')
+	}
+
+	function handleDelete(index) {
+		const newTodos = [...todos]
+		newTodos.splice(index, 1)
+		setTodos(newTodos)
+	}
+	return (
+		<div>
+			<h1>Todo List</h1>
+			<form>
+				<input type='text' value={inputValue} onChange={handleChange} />
+				<button onClick={handleSubmit}>Add Todo</button>
+			</form>
+			<ul>
+				{todos.map((todo, index) => (
+					<li key={index}>{todo}
+						<button onClick={() => handleDelete(index)}>Delete</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	)
+}
+
+export default PropsSender;
+// export default App;
+// export default TodoListFuntion;
